b73f21d8918c5c3d295cf837187a1622677342a4
eMMing <minh0987ae@gmail.com>
2025-06-20 15:15:08 +0700

    TOÁN RỜI RẠC 2

--- TRR2 Codeptit/TRR2001.cpp
+++ TRR2 Codeptit/TRR2001.cpp
@@ -1,62 +1,64 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define faster ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL); 
+const int maxN = 105;
 int t,n,u,v;
-int a[102][102]={0};
-int e [103];bool vs[103];
-void dfs (int u) {
-    vs[u]=false ;
-    for (int i=1;i<=n;i++) {
-        if (vs[i]==true ) {
-            if (a[u][i]==1) {
-                e[i]=u;
-               dfs (i);
-            }
+int a[maxN][maxN], par[maxN];
+vector<int> adj[maxN];
+bool visited[maxN];
+void dfs(int u){
+    visited[u] = 1;
+    for(int v:adj[u]){
+        if(!visited[v]){
+            par[v] = u;
+            dfs(v);
         }
     }
 }
-void solve () {
-    cin>>t>>n>>u>>v;
-    for (int i=1;i<=n;i++) {
-        for (int j=1;j<=n;j++) {
-            cin>>a[i][j];
-        }
+void path(int u,int v){
+    vector<int> res;
+    int s = v;
+    while(s!=u){
+       res.push_back(s); 
+       s = par[s];
     }
-    if (t==1) {
-        int count=0;
-        for (int i=1;i<=n;i++) {
-            if (a[u][i]==1 && a[i][v]==1) {
-                count++;
-            }
-        }
-        cout<<count<<endl;
-    }
-    else {
-        vector<int> mp;
-        for (int i=1;i<=n;i++) {
-            vs[i]=true;
-            e[i]=0;
-        }
-        dfs(u);
-        if (vs[v]==false) { 
-            int t=v;
-            while (t!=0) {
-                mp.push_back(t);
-                t=e[t];
-            }
-            for (int i=mp.size()-1;i>=0;i--) {
-                cout<<mp[i]<<" ";
-            }
-            cout<<endl;
-        }
-        else {
-            cout<<"0"<<endl;
-        }
+    res.push_back(u);
+    for(int i = res.size()-1;i>=0;i--){
+        cout<<res[i]<<" ";
     }
 }
 
+void solve(){
+    cin>>t>>n>>u>>v;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            cin>>a[i][j];
+            if(a[i][j]) adj[i].push_back(j);
+        }
+    }
+
+    if(t==1){
+        int cnt = 0;
+        for(int i=1;i<=n;i++){
+            if(a[u][i]==1 && a[i][v]==1){
+                cnt++;
+            }
+        }
+        cout<<cnt<<endl;
+    }
+    else{
+        dfs(u);
+        if(visited[v]){
+            path(u,v);
+        }
+        else{
+            cout<<0;
+        }
+    }
+}
 int main () {
+    faster;
     freopen("TK.INP","r",stdin);
     freopen("TK.OUT","w",stdout);
     solve();
-
 }
