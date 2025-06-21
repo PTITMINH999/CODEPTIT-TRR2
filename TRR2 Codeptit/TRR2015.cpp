057bcf2576b6f66543618a87ccd7b51aeb0fef7e
eMMing <minh0987ae@gmail.com>
2025-06-21 16:55:57 +0700

    TOÁN RỜI RẠC 2

--- TRR2 Codeptit/TRR2015.cpp
+++ TRR2 Codeptit/TRR2015.cpp
@@ -1,59 +1,68 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define faster ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+const int maxN = 105;
+int n,x,a[maxN][maxN];
+bool visited[maxN];
+vector<int> adj[maxN],rev_adj[maxN],adj2[maxN];
 
-const int MAX = 105;
-int n, a[MAX][MAX];
-bool visited[MAX];
-vector<int> adj[MAX];      
-vector<int> rev_adj[MAX];   
-vector<int> undirected[MAX]; 
-
-void dfs(int u, vector<int> g[]) {
-    visited[u] = true;
-    for (int v : g[u])
-        if (!visited[v]) dfs(v, g);
+void dfs(int u,vector<int> adj[]){
+    visited[u] = 1;
+    for(int v:adj[u]){
+        if(!visited[v]){
+            dfs(v,adj);
+        }
+    }
 }
 
-bool isStronglyConnected() {
-    memset(visited, false, sizeof visited);
-    dfs(1, adj);
-    for (int i = 1; i <= n; ++i)
-        if (!visited[i]) return false;
-    memset(visited, false, sizeof visited);
-    dfs(1, rev_adj);
-    for (int i = 1; i <= n; ++i)
-        if (!visited[i]) return false;
-
+bool LTManh(){
+    memset(visited,false,sizeof(visited));
+    dfs(1,adj);
+    for(int i=1;i<=n;i++){
+        if(!visited[i]){
+            return false;
+        }
+    }
+    memset(visited,false,sizeof(visited));
+    dfs(1,rev_adj);
+    for(int i=1;i<=n;i++){
+        if(!visited[i]){
+            return false;
+        }
+    }
     return true;
 }
 
-bool isWeaklyConnected() {
-    memset(visited, false, sizeof visited);
-    dfs(1, undirected);
-    for (int i = 1; i <= n; ++i)
-        if (!visited[i]) return false;
+bool LTYeu(){
+    memset(visited,false,sizeof(visited));
+    dfs(1,adj2);
+    for(int i=1;i<=n;i++){
+        if(!visited[i]){
+            return false;
+        }
+    }
     return true;
 }
-
+void solve(){
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            cin>>x;
+            if(x==1){
+                adj[i].push_back(j);
+                rev_adj[j].push_back(i);
+                adj2[i].push_back(j);
+                adj2[j].push_back(i);
+            } 
+        }
+    }
+    if(LTManh()) cout<<1;
+    else if(LTYeu()) cout<<2;
+    else cout<<0;
+}
 int main() {
+    faster;
     freopen("TK.INP", "r", stdin);
     freopen("TK.OUT", "w", stdout);
-
-    cin >> n;
-    for (int i = 1; i <= n; ++i)
-        for (int j = 1; j <= n; ++j) {
-            cin >> a[i][j];
-            if (a[i][j]) {
-                adj[i].push_back(j);        
-                rev_adj[j].push_back(i);     
-                undirected[i].push_back(j);  
-                undirected[j].push_back(i);  
-            }
-        }
-
-    if (isStronglyConnected()) cout << 1;
-    else if (isWeaklyConnected()) cout << 2;
-    else cout << 0;
-
-    return 0;
+    solve();
 }
